49ea3e081a0c8a59f6266dd13c78b5014bbd9c09
AdiZ-1579887 <simplemagictricks1@gmail.com>
2023-11-14 13:43:29 -0500

    Update Decoders.cs
    Nothing much changed.

--- Assets/Scripts/Decoders.cs
+++ Assets/Scripts/Decoders.cs
@@ -7,15 +7,15 @@ using UnityEngine;
 public class Decoders : MonoBehaviour
 {
     // Pre-defining the ordered alphabets and integers for ease of access in all functions that need it
-    string[] lowerAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    string[] upperAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    int[] integers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    string[] upperAlphabet = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    string[] lowerAlphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    int[] integers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     // Start is called before the first frame update
     void Start()
     {
         
-    }
+    }   
 
     // Update is called once per frame
     void Update()
@@ -29,17 +29,18 @@ public class Decoders : MonoBehaviour
         string decodedStr = "";
         foreach(char ch in encodedStr)
         {
-            // Digits remain unchanged (is this how the real cipher works?)
+            // Digits remain unchanged
             if(Char.IsDigit(ch))
             {
                 decodedStr += ch;
             } else if(Char.IsUpper(ch))
             {
-                // Move the array 'shuffle' times in the upperAlphabet array
-            } else
+                newCh = upperAlphabet[(Array.IndexOf(ch) + shuffle)];
+            } else if(Char.IsLower(ch))
             {
                 // Here, the character must be a lower case letter in the alphabet
                 // Move the array 'shuffle' times in the lowerAlphabet array
+                int x = 0;
             }
         }
         return decodedStr;
