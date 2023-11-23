82efde75ac8e9a0064c23d3974856190d7653913
AdiZ-1579887 <simplemagictricks1@gmail.com>
2023-11-22 20:26:20 -0500

    Decode Button UP AND WORKING!

--- Assets/Scripts/DecodeButton.cs
+++ Assets/Scripts/DecodeButton.cs
@@ -1,31 +1,24 @@
 using System;
+using TMPro;
 using UnityEngine;
 
 public class DecodeButton : MonoBehaviour
 {
-    ReadStringInput plaintextStringInput;
-    ReadStringInput shuffleStringInput;
+    [SerializeField] TMP_InputField plaintextInputField;
+    [SerializeField] TMP_InputField shuffletextInputField;
 
     Decoders decoderScript;
 
     void Awake()
     {
-        // Instantiate GameObjects for each of the text box inputs (for the plaintext and the shuffle).
-        GameObject plaintextGameObject = new GameObject("ReadStringInput");
-        plaintextStringInput = gameObject.AddComponent<ReadStringInput>();
-
-        GameObject shuffleGameObject = new GameObject("ReadStringInput");
-        shuffleStringInput = gameObject.AddComponent<ReadStringInput>();
-
-        // Instantiation for usage of other scripts
-        GameObject decoderGameObject = new GameObject("Decoders");
-        decoderScript = gameObject.AddComponent<Decoders>();
+        
     }
 
-    void OnClick()
+    public void OnClick()
     {
-        int shuffle;
-        Int32.TryParse(shuffleStringInput.input, out shuffle);
-        string output = decoderScript.CaesarCipher(plaintextStringInput.input, shuffle);
+        // int shuffle;
+        Debug.Log(plaintextInputField.text);
+        // Int32.TryParse(shuffleStringInput.input, out shuffle);
+        // string output = decoderScript.CaesarCipher(plaintextStringInput.input, shuffle);
     }
 }
