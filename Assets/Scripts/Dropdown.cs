d5ae41716d9919f51bf51ab0815aa3e4cc13682e
AdiZ-CSharp <ztech58@gmail.com>
2023-11-28 13:40:45 -0500

    WORKING - Added GameObject holder for Dropdown.cs

--- Assets/Scripts/Dropdown.cs
+++ Assets/Scripts/Dropdown.cs
@@ -3,17 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class Dropdown
+public class Dropdown : MonoBehaviour
 {
-    public Dropdown()
-    {
-
-    }
-
     [SerializeField] TMP_Dropdown dropDownObject;
     TMP_Text dropDownValue;
     
-    void OnValueChanged()
+    public void OnValueChanged(int index)
     {
         dropDownValue = dropDownObject.captionText;
     }
